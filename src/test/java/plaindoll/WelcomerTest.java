477590630e98c3606cda04574c8c9ac7b5e087fa
TSA <rrchip@ya.ru>
2026-09-24 14:36:27 +0300

    hunt

--- src/test/java/plaindoll/WelcomerTest.java
+++ src/test/java/plaindoll/WelcomerTest.java
@@ -32,4 +32,8 @@ public class WelcomerTest {
 	public void welcomerSaysSomething(){
 		assertThat(welcomer.saySome(), containsString("something"));
 	}
+	@Test
+	public void welcomersayByeHunter(){
+		assertThat(welcomer.sayByeHunter(), containsString("hunter"));
+	}
 }
